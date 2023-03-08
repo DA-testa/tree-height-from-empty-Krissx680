1058c24ac691eda37bb6c16ba5f66503f8339ce7
221RDB362 <122871094+Krissx680@users.noreply.github.com>
2023-03-08 22:29:18 +0300

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -6,33 +6,32 @@ import numpy
 
 
 def compute_height(n, parents):
-   children = {}
+    children = {}
     for i in range(n):
         children[i] = []
-    for i, parent in enumerate(parents)
-        if parent == -1
+    for i, parent in enumerate(parents):
+        if parent == -1:
             root = i
         else:
             children[parent].append(i)
-    def calc_height(node)
-        if not children[node]
+    def calc_height(node):
+        if not children[node]:
             return 1
         else:
             return 1+max([calc_height(child) for child in children[node]])
     return calc_height(root)
 
-
 def main():
-     source = input().strip()
+    source = input().strip()
     if source == "I":
         n = int(input().strip())
         parents = list(map(int, input().strip().split()))
     elif source == "F":
         file_name = input().strip()
-        if "a" in file_name
+        if "a" in file_name:
             sys.exit()
         try:
-            with open(file_name, "r") as file
+            with open(file_name, "r") as file:
                 n = int(file.readline().strip())
                 parents = list(map(int, file.readline().strip().split()))
         except FileNotFoundError:
